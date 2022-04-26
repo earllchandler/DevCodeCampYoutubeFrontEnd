c1a60297349275fc3a908f07be95c230d8797ad1
Earl Chandler <earllchandler@gmail.com>
2022-04-26 09:07:00 -0400

    added function in app.jsx file that will display comments likes and dislikes of videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+
+
 
 function App() {
+  useEffect(()=> {
+    getAllComments();
+  },[])
+  async function getAllComments(){
+    try{
+      let response = await axios.get('http://localhost:3009/api/comments')
+      console.log(response.data);
+    } 
+    catch(err){
+      console.console.log(err.message)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
